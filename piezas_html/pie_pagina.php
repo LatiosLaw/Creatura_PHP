151619ca30cda5192012630451a172621ed363d6
Tabare8031 <facundovuerra@gmail.com>
2025-06-17 01:36:46 -0300

    Ya no me da cáncer la cabecera + footer
    Y adicionalmente, resulta que si habrán hojas de estilo sin que se rompa feo
    ^u^

--- piezas_html/pie_pagina.php
+++ piezas_html/pie_pagina.php
@@ -4,19 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>-</title>
-    <style>
-        *{
-            margin: 0;
-            padding: 0;
-            font-family: Arial, sans-serif;
-        }
-    </style>
+    <link rel="stylesheet" href="\Creatura_PHP\styles\pie_pagina.css">
 </head>
 <body>
-    <footer>HIJOEPUTA - 
-
-CONCHASUMARE
-
-</footer>
+    <footer class="pie-pagina">
+        <img class="img-footer" src="\Creatura_PHP\imagenes\UTEC_logo.png" alt="Logo UTEC">
+        <div class="division-footer div-foot">
+            <ul class="listado-footer">
+                <li>Tecnologo en Informatica</li>
+                <li>Taller de Sistemas de Informacion PHP</li>
+            </ul>
+        </div>
+        <div class="division-footer">
+            <h3>Integrantes</h3>
+            <ul class="listado-footer">
+                <li>Andres Ferreira - 5.529.445-0</li>
+                <li>Facundo Guerra - 5.619.400-1</li>
+                <li>Martin Mainentti - 5.488.144-0</li>
+            </ul>
+        </div>
+    </footer>
 </body>
 </html>
