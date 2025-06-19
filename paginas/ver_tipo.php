060f7831219f8f93dab3ae7077c7cac04c225962
Tabare8031 <facundovuerra@gmail.com>
2025-06-19 20:19:07 -0300

    Ver habilidad
    Beneficios del reciclaje:
    El reciclaje ofrece múltiples beneficios, incluyendo la reducción de la contaminación, la conservación de recursos naturales, el ahorro de energía y la disminución de residuos. También contribuye a la economía circular y genera empleo en las comunidades.

--- paginas/ver_tipo.php
+++ paginas/ver_tipo.php
@@ -164,44 +164,44 @@ $habilidades_tipo = $controladorTipo->retornar_habilidades_tipo($id_tipo);
 <div class="contenedor-creaturas">
     <?php while ($fila = mysqli_fetch_assoc($criaturas_tipo) ) :
         $tipo1 = $controladorTipo->retornar_tipo($fila['id_tipo1']);
-            $tipo2 = $controladorTipo->retornar_tipo($fila['id_tipo2'])
-        ?>
-        <div class="contenido-creatura" onclick="window.location.href='/Creatura_PHP/paginas/ver_creatura.php?creatura=<?= urlencode($fila['nombre_creatura']) ?>&creador=SYSTEM'">
-            <div class="imagen-creatura">
-                <img src="/Creatura_PHP/imagenes/creaturas/<?= htmlspecialchars($fila['imagen']) ?>" alt="Imagen" onerror="this.onerror=null; this.src='/Creatura_PHP/imagenes/sin_imagen.png';">
-            </div>
-            <div class="nombre-creatura">
-                <?= htmlspecialchars($fila['nombre_creatura']) ?>
-            </div>
-            <div class="tipos">
-                <?php if ($fila['id_tipo1'] != 0): ?>
-                    <a href="/Creatura_PHP/paginas/ver_tipo.php?nombre_tipo=<?= urlencode($tipo1['nombre_tipo']) ?>&creador=<?= urlencode($tipo1['creador'])?>&id_tipo=<?= urlencode($tipo1['id_tipo'])?>">
-                        <img src="/Creatura_PHP/imagenes/tipos/<?= $tipo1['icono']; ?>"
-                        <?php if($tipo1['icono'] == "sin_icono.png"): ?>
-                            style="background-color: #<?= $tipo1['color']; ?>
-                        <?php endif; ?>
-                        ;" alt="<?= $tipo1['nombre_tipo']; ?>" onerror="this.onerror=null; this.src='/Creatura_PHP/imagenes/sin_imagen.png';">
-                    </a>
-                <?php endif; ?>
-                <?php if ($fila['id_tipo2'] != 0): ?>
-                    <a href="/Creatura_PHP/paginas/ver_tipo.php?nombre_tipo=<?= urlencode($tipo2['nombre_tipo']) ?>&creador=<?= urlencode($tipo2['creador'])?>&id_tipo=<?= urlencode($tipo2['id_tipo'])?>">
-                        <img src="/Creatura_PHP/imagenes/tipos/<?= $tipo2['icono']; ?>" 
-                        <?php if($tipo2['icono'] == "sin_icono.png"): ?>
-                            style="background-color: #<?= $tipo2['color']; ?>
-                        <?php endif; ?>
-                        ;" alt="<?= $tipo2['nombre_tipo']; ?>" onerror="this.onerror=null; this.src='/Creatura_PHP/imagenes/sin_imagen.png';">
-                    </a>
-                <?php endif; ?>
-            </div>
-            <div class="rating">
-                <?= htmlspecialchars($controladorCreatura->rating_promedio(($fila['id_creatura']))) ?>/5 de puntuación
-            </div>
-            <div class="creador-creatura">
-                <?= htmlspecialchars($fila['creador']) ?>
-            </div>
+        $tipo2 = $controladorTipo->retornar_tipo($fila['id_tipo2'])
+    ?>
+    <div class="contenido-creatura" onclick="window.location.href='/Creatura_PHP/paginas/ver_creatura.php?creatura=<?= urlencode($fila['nombre_creatura']) ?>&creador=SYSTEM'">
+        <div class="imagen-creatura">
+            <img src="/Creatura_PHP/imagenes/creaturas/<?= htmlspecialchars($fila['imagen']) ?>" alt="Imagen" onerror="this.onerror=null; this.src='/Creatura_PHP/imagenes/sin_imagen.png';">
         </div>
-        <?php endwhile; ?>
+        <div class="nombre-creatura">
+            <?= htmlspecialchars($fila['nombre_creatura']) ?>
+        </div>
+        <div class="tipos">
+            <?php if ($fila['id_tipo1'] != 0): ?>
+                <a href="/Creatura_PHP/paginas/ver_tipo.php?nombre_tipo=<?= urlencode($tipo1['nombre_tipo']) ?>&creador=<?= urlencode($tipo1['creador'])?>&id_tipo=<?= urlencode($tipo1['id_tipo'])?>">
+                    <img src="/Creatura_PHP/imagenes/tipos/<?= $tipo1['icono']; ?>"
+                    <?php if($tipo1['icono'] == "sin_icono.png"): ?>
+                        style="background-color: #<?= $tipo1['color']; ?>
+                    <?php endif; ?>
+                    ;" alt="<?= $tipo1['nombre_tipo']; ?>" onerror="this.onerror=null; this.src='/Creatura_PHP/imagenes/sin_imagen.png';">
+                </a>
+            <?php endif; ?>
+            <?php if ($fila['id_tipo2'] != 0): ?>
+                <a href="/Creatura_PHP/paginas/ver_tipo.php?nombre_tipo=<?= urlencode($tipo2['nombre_tipo']) ?>&creador=<?= urlencode($tipo2['creador'])?>&id_tipo=<?= urlencode($tipo2['id_tipo'])?>">
+                    <img src="/Creatura_PHP/imagenes/tipos/<?= $tipo2['icono']; ?>" 
+                    <?php if($tipo2['icono'] == "sin_icono.png"): ?>
+                        style="background-color: #<?= $tipo2['color']; ?>
+                    <?php endif; ?>
+                    ;" alt="<?= $tipo2['nombre_tipo']; ?>" onerror="this.onerror=null; this.src='/Creatura_PHP/imagenes/sin_imagen.png';">
+                </a>
+            <?php endif; ?>
+        </div>
+        <div class="rating">
+            <?= htmlspecialchars($controladorCreatura->rating_promedio(($fila['id_creatura']))) ?>/5 de puntuación
+        </div>
+        <div class="creador-creatura">
+            <?= htmlspecialchars($fila['creador']) ?>
+        </div>
     </div>
+    <?php endwhile; ?>
+</div>
 <?php endif; ?>
 
 <?php include_once("../piezas_html/pie_pagina.php"); ?>
